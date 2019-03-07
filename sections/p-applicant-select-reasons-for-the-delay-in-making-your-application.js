099e0a05e919decd5a71bb310fdd346660775790
Stephen Sinclair <Stephen.Sinclair@CICA.GOV.UK>
2019-03-07 17:57:22 +0000

    Remove 'classification' and 'rows' properties from sections

--- sections/p-applicant-select-reasons-for-the-delay-in-making-your-application.js
+++ sections/p-applicant-select-reasons-for-the-delay-in-making-your-application.js
@@ -1,42 +1,40 @@
 const output = {
-  "$schema": "http://json-schema.org/draft-07/schema#",
-  "type": "object",
-  "required": ["q-applicant-reason-for-delay-in-application"],
-  "additionalProperties": false,
-  "properties": {
-    "q-applicant-reason-for-delay-in-application": {
-      "title": "Select reasons for the delay in making your application",
-      "type": "array",
-      "anyOf": [
-        {
-          "title": "I was under 18",
-          "const": "underage"
+    $schema: 'http://json-schema.org/draft-07/schema#',
+    type: 'object',
+    required: ['q-applicant-reason-for-delay-in-application'],
+    additionalProperties: false,
+    properties: {
+        'q-applicant-reason-for-delay-in-application': {
+            title: 'Select reasons for the delay in making your application',
+            type: 'array',
+            anyOf: [
+                {
+                    title: 'I was under 18',
+                    const: 'underage'
+                },
+                {
+                    title: 'I was advised to wait',
+                    const: 'wait'
+                },
+                {
+                    title: 'Medical reasons',
+                    const: 'med'
+                },
+                {
+                    title: 'Other reasons',
+                    const: 'other'
+                }
+            ],
+            errorMessages: {
+                required: 'Select your choice'
+            }
         },
-        {
-          "title": "I was advised to wait",
-          "const": "wait"
-        },
-        {
-          "title": "Medical reasons",
-          "const": "med"
-        },
-        {
-          "title": "Other reasons",
-          "const": "other"
+        'q-applicant-explain-delay-reasons': {
+            title: 'Briefly explain these reasons',
+            type: 'string',
+            maxLength: 500
         }
-      ],
-      "errorMessages": {
-        "required": "Select your choice"
-      }
-    },
-    "q-applicant-explain-delay-reasons": {
-      "title": "Briefly explain these reasons",
-      "type": "string",
-      "classification": "textarea",
-      "rows": "5"
     }
-  }
 };
 
 module.exports = output;
-
--- sections/p-offender-describe-contact-with-offender.js
+++ sections/p-offender-describe-contact-with-offender.js
@@ -1,24 +1,23 @@
 const output = {
-  "$schema": "http://json-schema.org/draft-07/schema#",
-  "type": "object",
-  "additionalProperties": false,
-  "properties": {
-    "q-offender-contact-description": {
-      "type": "string",
-      "title": "If you have contact with the offender, describe it below",
-      "classification": "textarea",
-      "rows": "5"
-    },
-    "q-offender-no-contact-with-offender": {
-      "type": "array",
-      "oneOf": [
-        {
-          "title": "I have no contact with the offender",
-          "const": "none"
+    $schema: 'http://json-schema.org/draft-07/schema#',
+    type: 'object',
+    additionalProperties: false,
+    properties: {
+        'q-offender-contact-description': {
+            type: 'string',
+            title: 'If you have contact with the offender, describe it below',
+            maxLength: 500
+        },
+        'q-offender-no-contact-with-offender': {
+            type: 'array',
+            oneOf: [
+                {
+                    title: 'I have no contact with the offender',
+                    const: 'none'
+                }
+            ]
         }
-      ]
     }
-  }
 };
 
 module.exports = output;
