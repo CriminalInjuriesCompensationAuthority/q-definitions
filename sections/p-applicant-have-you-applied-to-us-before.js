fd0206b0ca2e1b59c7d0692ec901f013c3581f48
Stephen Sinclair <Stephen.Sinclair@CICA.GOV.UK>
2019-03-12 13:17:07 +0000

    Update p-applicant-have-you-applied-to-us-before schema to use boolean values

--- sections/p-applicant-have-you-applied-to-us-before.js
+++ sections/p-applicant-have-you-applied-to-us-before.js
@@ -1,25 +1,48 @@
 const output = {
-  "$schema": "http://json-schema.org/draft-07/schema#",
-  "type": "object",
-  "required": ["q-applicant-have-you-applied-to-us-before"],
-  "additionalProperties": false,
-  "properties": {
-    "q-applicant-have-you-applied-to-us-before": {
-      "title": "Have you applied to us before?",
-      "type": "string",
-      "oneOf": [
-        {
-          "title": "Yes",
-          "const": "Yes"
+    $schema: 'http://json-schema.org/draft-07/schema#',
+    type: 'object',
+    propertyNames: {
+        enum: [
+            'q-applicant-have-you-applied-to-us-before',
+            'q-enter-your-previous-reference-number'
+        ]
+    },
+    properties: {
+        'q-applicant-have-you-applied-to-us-before': {
+            title: 'Have you applied to us before?',
+            type: 'boolean'
         },
-        {
-          "title": "No",
-          "const": "No"
+        'q-enter-your-previous-reference-number': {
+            type: 'string',
+            title: 'Enter your previous reference number if you know it (optional)'
         }
-      ]
+    },
+    required: ['q-applicant-have-you-applied-to-us-before'],
+    allOf: [
+        {
+            $ref: '#/definitions/if-true-then-q-enter-your-previous-reference-number-is-required'
+        }
+    ],
+    definitions: {
+        'if-true-then-q-enter-your-previous-reference-number-is-required': {
+            if: {
+                properties: {
+                    'q-applicant-have-you-applied-to-us-before': {
+                        const: true
+                    }
+                }
+            },
+            then: {
+                required: ['q-enter-your-previous-reference-number'],
+                propertyNames: {
+                    enum: [
+                        'q-applicant-have-you-applied-to-us-before',
+                        'q-enter-your-previous-reference-number'
+                    ]
+                }
+            }
+        }
     }
-  }
 };
 
 module.exports = output;
-
