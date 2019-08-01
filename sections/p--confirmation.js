7bc0cc49b1af3a8c386875f0fd134c6cb5bec4e6
Barry Piccinni <barry.piccinni@CICA.GOV.UK>
2019-08-01 12:04:58 +0100

    Fix incorrect syntax

--- sections/p--confirmation.js
+++ sections/p--confirmation.js
@@ -7,7 +7,7 @@ const output = {
             description: `
                     {{ govukPanel({
                         titleText: "Application submitted",
-                        html: '<p>Your reference number is <strong>||#/answers/system/case-reference||</strong></p><p>We have sent a confirmation email to <strong>{{$.answers.p-applicant-enter-your-email-address.q-applicant-email-address}}</strong></p>'
+                        html: '<p>Your reference number is <strong>||#/answers/system/case-reference||</strong></p><p>We have sent a confirmation email to <strong>||#/answers/p-applicant-enter-your-email-address/q-applicant-email-address||</strong></p>'
                     }) }}
                     
                     <p class="govuk-body-l">Thank you for submitting your application.</p>
