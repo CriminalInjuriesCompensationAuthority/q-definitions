af1cbe9959914924352b620752e8caa7ccb7c841
Adrian Roworth <adrian.roworth@CICA.GOV.UK>
2019-08-01 16:20:13 +0100

    Update case reference regex to cater for escaping needs

--- sections/system.js
+++ sections/system.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const output = {
     $schema: 'http://json-schema.org/draft-07/schema#',
     type: 'object',
@@ -6,7 +8,7 @@ const output = {
     properties: {
         'case-reference': {
             type: 'string',
-            pattern: '^[0-9]{2}\\[0-9]{6}$',
+            pattern: '^[0-9]{2}\\\\[0-9]{6}$',
             errorMessage: {
                 pattern: 'Invalid case reference'
             }
